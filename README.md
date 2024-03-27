39f6c9c4fabeff1417d14321ddde872502b6f85c
yleniapregnolato <yleniapregnolato@gmail.com>
2024-03-27 16:35:28 +0100

    fix: readme, gap in box and tex in box

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 1. stampare in console i numeri da 1 a 100
 2. per i multipli di 3 stampare "Fizz" al posto del numero, per i multipli di 5 stampare "Buzz", per i multipli sia di 3 che di 5 stampare "FizzBuzz".
 3. creare una griglia di numeri nel dom e assegnare le classi corrispondenti in base al tipo di numero (fizz, buzz o fizzbuzz).
+4. stampare i testi nelle celle:
+- creiamo variabile 
+- assegnamo il valore corretto alla variabile in base al valore di "i"
+- usiamo la variabile nella stringa HTML
--- js/script.js
+++ js/script.js
@@ -5,26 +5,31 @@ console.log(rowElem);
 // creo un ciclo for per numeri da 1 a 100 e lo stampo in console
 for (let i = 1; i <= 100; i++) {
     let bgClass;
+    let text;
     // per i numeri divisibili sia per 3 che per 5
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
         bgClass = "fizzbuzz"
+        text = "FizzBuzz";
         // per i numeri divisibili per 3
     } else if (i % 3 === 0) {
         console.log("Fizz");
         bgClass = "fizz";
+        text = "Fizz";
         // per i numeri divisibili per 5
     } else if (i % 5 === 0) {
         console.log("Buzz");
         bgClass = "buzz"
+        text = "Buzz";
         // per tutti gli altri numeri
     } else {
         console.log(i);
         bgClass = "number";
+        text = i;
     }
 
     // stampo i risultati nel DOM
-    const boxElem = `<div class="box ${bgClass}">${i}</div>`; // string
+    const boxElem = `<div class="box ${bgClass}">${text}</div>`; // string
     rowElem.innerHTML += boxElem;
 }
 
